46f4cbc0168112ea6bc411fc5f32baa134a9fe78
Bekah Ayres <rebekah.ayres.19@ucl.ac.uk>
2020-02-19 19:24:07 +0000

    renaming variables to be more explicit

--- clustering.py
+++ clustering.py
@@ -3,28 +3,28 @@ from random import *
 
 k=3
 
-lines = open('samples.csv', 'r').readlines()
-ps=[]
-for line in lines: ps.append(tuple(map(float, line.strip().split(','))))
+lines = open('data/samples.csv', 'r').readlines()
+points=[]
+for line in lines: points.append(tuple(map(float, line.strip().split(','))))
 
-m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
+cluster_centers=[points[randrange(len(points))], points[randrange(len(points))], points[randrange(len(points))]]
 
-alloc=[None]*len(ps)
-n=0
-while n<10:
-  for i in range(len(ps)):
-    p=ps[i]
-    d=[None] * 3
-    d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
-    d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
-    d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
-    alloc[i]=d.index(min(d))
-  for i in range(3):
-    alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-    new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
-    m[i]=new_mean
-  n=n+1
+cluster_num_allocations=[None]*len(points)
+iteration_num=0
+while iteration_num<10:
+  for point_index in range(len(points)):
+    point = points[point_index]
+    distances_to_clusters = [None] * 3
+    distances_to_clusters[0] = sqrt((point[0]-cluster_centers[0][0])**2 + (point[1]-cluster_centers[0][1])**2)
+    distances_to_clusters[1] = sqrt((point[0]-cluster_centers[1][0])**2 + (point[1]-cluster_centers[1][1])**2)
+    distances_to_clusters[2] = sqrt((point[0]-cluster_centers[2][0])**2 + (point[1]-cluster_centers[2][1])**2)
+    cluster_num_allocations[point_index] = distances_to_clusters.index(min(distances_to_clusters))
+  for cluster_num in range(3):
+    points_in_cluster=[point for j, point in enumerate(points) if cluster_num_allocations[j] == cluster_num]
+    new_mean=(sum([point[0] for point in points_in_cluster]) / len(points_in_cluster), sum([point[1] for point in points_in_cluster]) / len(points_in_cluster))
+    cluster_centers[cluster_num]=new_mean
+  iteration_num=iteration_num+1
 
-for i in range(3):
-  alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-  print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
+for cluster_num in range(3):
+  points_in_cluster=[point for j, point in enumerate(points) if cluster_num_allocations[j] == cluster_num]
+  print("Cluster " + str(cluster_num) + " is centred at " + str(cluster_centers[cluster_num ]) + " and has " + str(len(points_in_cluster)) + " points.")
